34c107f58d90b61af5efe076bb0ba731ee932957
Ilia Loviagin <ilia.loviagin@gmail.com>
2025-08-30 00:30:04 +0300

    Working version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.lovigin.android.allbanks"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.1"
+        versionCode = 2
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
